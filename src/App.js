61e288e93c4abd7e3f26f881b8300da409855d52
Mahek <chadhataranpreet02@gmail.com>
2021-05-31 02:23:34 +0530

    made a signup component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import { Switch , Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop-page/shop.jsx';
+import Header from './components/header/header.jsx';
+import SignPage from './pages/sign-page/sign.jsx';
 
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage}  />
+      <Route path='/signin' component={SignPage}  />
+
     </Switch>
       
     </div>
